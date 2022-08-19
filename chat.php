e769ccb09fc73e491ca1cec65428c72f3895e908
hasan <h.faracov321@gmail.com>
2022-08-20 01:50:08 +0400

    .

--- chat.php
+++ chat.php
@@ -16,7 +16,7 @@ include_once "header.php";
                 <?php
                     include_once "php/config.php";
                     $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
-                    $sql =mysqli_query($conn, "Select * from Users where unique_id={$user_id}");
+                    $sql =mysqli_query($conn, "Select * from users where unique_id={$user_id}");
                     if( mysqli_num_rows($sql)> 0){
                         $row = mysqli_fetch_assoc($sql);
                     }
